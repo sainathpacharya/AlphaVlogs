ab3c62b07772712840e782651abe63393e38cca3
Naga Sainath Reddy Palle <nagasainath.palle@ratnaglobaltech.com>
2025-06-10 13:54:18 +0530

    Initial packages

---
+++ src/navigation/AppStack/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const AppStack = createNativeStackNavigator();
+
+const AppStackScreen = () => {
+  return <></>;
+};
+
+export {AppStack, AppStackScreen};
---
+++ src/navigation/AuthStack/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const AuthStack = createNativeStackNavigator();
+
+const AuthStackScreen = () => {
+  return <></>;
+};
+
+export {AuthStack, AuthStackScreen};
